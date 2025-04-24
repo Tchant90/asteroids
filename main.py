843dc4c43d6fe21516de393951b9501b708a8e93
Tchant90 <travis.chant@gmail.com>
2025-04-24 17:20:31 -0400

    Got the window setup working, added a .gitignore

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(black)
+        pygame.display.flip()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
